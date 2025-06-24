6d031cf31e2ef2b42d5f5d6ce74e50734584ac36
Emmanuel Chukwuemetam Michael <immanuel@meta.com>
2025-06-23 17:45:41 -0700

    I created my database and my server. Also made sure everything is in sync

---
+++ client/client/src/Components/Login.jsx
@@ -0,0 +1,13 @@
+import './Login.css'
+import { useState } from 'react'
+
+export default function Login() {
+
+    return (
+        <div>
+            <form className='login-form'>
+                
+            </form>
+        </div>
+    )
+}
