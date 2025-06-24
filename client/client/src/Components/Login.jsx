a3ac2e6dd83913ffd4edf3509aa9f28c548cdb41
Emmanuel Chukwuemetam Michael <immanuel@meta.com>
2025-06-24 11:06:02 -0700

    made change on my database- added the username with a unique constraint. created the signup auth

--- client/client/src/Components/Login.jsx
+++ client/client/src/Components/Login.jsx
@@ -6,7 +6,18 @@ export default function Login() {
     return (
         <div>
             <form className='login-form'>
-                
+                <label htmlFor="email">Email:</label>
+                <input id="email" type="text" name="email" placeholder="immanuel@meta.com" />
+                <br />
+
+                <label htmlFor="password">Password:</label>
+                <input id="password" type="password" name="password"/>
+                <br />
+
+                <button>Login</button>
+
+                <p>Forgot password?</p>
+                <p>SignUp here</p>
             </form>
         </div>
     )
--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient() 
+import express  from 'express';
+import cors from "cors"
 
-const express = require('express');
+import authRouter from "./routes/auth.js"
+
+
 const app = express();
 const PORT = 3007;
+
 app.use(express.json());
-app.listen(PORT, () => {
+app.use(cors())
+
+
+app.use("/auth", authRouter);
+
+
+app.listen(PORT, () => { 
     console.log(`Server listening on port ${PORT}`);
-});
+}); 
