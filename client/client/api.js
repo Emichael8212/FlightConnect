f4565a129d4e68eb24cf84622c14031897e04b74
Emichael8212 <146501429+Emichael8212@users.noreply.github.com>
2025-06-26 20:33:15 -0700

    Authentication for login (#2)
    * made change on my database- added the username with a unique constraint. created the signup auth
    
    * implemented the router for login aiuthentication of user
    
    * Set up login and register components to handle user input and send it to the server via req.body. Also added api.js to manage Axios requests from the frontend.
    
    * trying to isolate an issue. will still modify
    
    * implemented authentication with JWT
    
    ---------
    
    Co-authored-by: Emmanuel Chukwuemetam Michael <immanuel@meta.com>

---
+++ client/client/api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+
+// registration
+export const registerUser = async (formData) => {
+    try {
+        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/auth/register`, formData);
+        return {wasSuccessful: true, data: response.data};
+    }   catch (error) {
+        const message = error.response && error.response.data && 
+        error.response.data.error ? error.response.data.error : "Registration failed";
+        return {wasSuccessful: false, error: message};
+    }
+}
+
+
+export const loginUser = async (formData) => {
+    try {
+        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/auth/login`, formData);
+        return {wasSuccessful: true, data: response.data};
+    }   catch (error) {
+        const message = error.response && error.response.data && 
+        error.response.data.error ? error.response.data.error : "Login failed";
+        return {wasSuccessful: false, error: message};
+    }
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,21 +1,29 @@
 import express from "express"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
 import { PrismaClient } from '@prisma/client'
+import { authenticateToken } from "./authMiddleware.js";
 
-const prisma = new PrismaClient() 
+
 const router = express.Router()
+const prisma = new PrismaClient() 
+
 
 router.post("/register", async(req, res) => {
     try {
-        const {username, email, password } = req.body
+        const {username, email, password, confirmPassword } = req.body
         console.log("Received body", req.body)
 
-        if (!username || !password) {
-            return res.status(400).json({error: "Username is required"})
+        if (!username || !password || !email || !confirmPassword) {
+            return res.status(400).json({error: "Fill all fields"})
         }
 
         if (password.length < 8) {
             return res.status(400).json({error: "Password must be at least 8 characters long."})
+        }
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({error: "Passwords do not match"})
         }
 
         const existingUser = await prisma.user.findUnique({
@@ -39,12 +47,62 @@ router.post("/register", async(req, res) => {
         
 
         res.status(201).json({message: "Signup successful!", 
-            user: { id: newUser.id, username: newUser.username, email: newUser.email}});
+            user: { 
+                id: newUser.id, 
+                username: newUser.username, 
+                email: newUser.email}});
     }   catch   (error) {
         console.error(error)
         res.status(500).json({error: "Something went wrong during signup"})
     }
 })
 
-export default router
+// Login authentication:
+router.post("/login", async(req, res) => {
+    // get the username and password from the login form
+    const {username, password} = req.body
+    try {   
+            // check if user provides the username and password
+            if (!username || !password) {
+                res.status(400).json({error: "Username and Password Required"})
+            }
+            // look up user by their username
+            const user = await prisma.user.findUnique({
+                where: {username}
+            });
+            if (!user) {
+                res.status(400).json({error: "Invalid Username or Password Required"})
+            }
+            // compared user provided password with one in my database
+            const isValidPassword = await bcrypt.compare(password, user.password);
 
+            if (!isValidPassword) {
+                res.status(400).json({error: "Incorrect Username or Password"})
+            }
+
+            const token = jwt.sign({
+                userId: user.id, username: user.username},
+                process.env.JWT_SECRET_KEY,
+                {expiresIn: process.env.JWT_EXPIRES}
+            )
+
+            res.cookie("token", token, {
+                httpOnly: true,
+                sameSite: "strict",
+                secure: process.env.NODE_ENV === "production",
+                maxAge: 3*60*60*1000
+            })
+
+            return res.status(200).json({message: "Login Successful"})
+    }   catch (error) {
+        console.error(error)
+    }
+})
+
+
+router.get("/profile", authenticateToken, async (req, res) => {
+    const {userId, username} = req.user
+    res.json({userId, username})                                                            
+})
+
+export default router
