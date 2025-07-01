0e4d46f1cb21b6c01cbb4df2e89adbb7f601f641
Emmanuel Chukwuemetam Michael <immanuel@meta.com>
2025-07-01 12:03:53 -0700

    commited my updated app.js and main.js

--- client/client/src/main.jsx
+++ client/client/src/main.jsx
@@ -1,34 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import Register from './Components/Register.jsx';
-import Login from './Components/Login.jsx'
+import AuthenticationContextProvider from './Context/AuthenticationContext.jsx'
 
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <div>Not Found</div>
-  }, 
-
-  {
-    path: "/auth/register",
-    element: <Register />
-  },
-
-  {
-    path: "/auth/login",
-    element: <Login />
-  }
-]);
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />  
+    <AuthenticationContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthenticationContextProvider>
   </StrictMode>,
 )
