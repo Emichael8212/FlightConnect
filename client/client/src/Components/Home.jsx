404ec926fe746f3d0130fe70e7798ce0fd98e632
Emmanuel Chukwuemetam Michael <immanuel@meta.com>
2025-07-01 02:47:19 -0700

    Integrate Aviation API at /flights route and add filter functionality to return relevant data based on user formdata

--- client/client/src/Components/Home.jsx
+++ client/client/src/Components/Home.jsx
@@ -1,11 +1,65 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 
 export default function Home() {
+    const [setUser, isSetUser] = useState("");
+    const [searchType, setSearchType] = useState("route")
+    const [flightFoem, setFlightForm] = useState({
+        airline: "",
+        date: "",
+        origin: "",
+        destination: ""
+    })
+
+    useEffect(() => {
+        const getUser = async () => {
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/auth/profile`,
+                    {
+                        withCredentials: true
+                    }
+                );
+                getUser = response.data
+            }   catch (error) {
+                console.error("User not Authenticated")
+            }       
+        };
+
+        getUser();
+    }, [])
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFlightForm(prev => ({...prev, [name]: value}));
+    };
+
+    // handle subnission
+    const handleSearch = (event) => {
+        event.preventDefault();
+
+        if (searchType === "flight") {
+            
+        }
+    }
+
+
+
 
     return (
         <main>
-            <H1>WELCOME EMMAN</H1>
+            <div className="flight-container">
+                <h2>WELCOME {username}</h2>
+                <p>Connect with your loved ones as they travel.</p>
+                <section className="track-searchbar">
+                    <div className="search-btns">
+                        <button className="flight-btn">By Flight number</button>
+                        <button className="route-btn">By Route</button>
+                    </div>
+                    <form action=""></form>
+                    
+                </section>
+            </div>
         </main>
     )
 }
