4eb6b6df39e5474474c476305ed22e109e02ed08
Emmanuel Chukwuemetam Michael <immanuel@meta.com>
2025-07-06 18:55:03 -0700

    logout route to clear user token

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -103,4 +103,13 @@ router.get("/auth/profile", authenticateToken, async (req, res) => {
     res.json({userId, username})
 })
 
+router.post("/logout", (req, res) => {
+    res.clearCookie("token", {
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+    });
+    return res.json({message: "Logout Successful"});
+});
+
 export default router
