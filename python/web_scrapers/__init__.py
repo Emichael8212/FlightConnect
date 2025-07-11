41f07a01bdf04d0a008a56ba09e0b92f77c4ec4f
Emmanuel Chukwuemetam Michael <immanuel@meta.com>
2025-07-11 15:53:09 -0700

    initialized RestaurantScraper as a package

--- python/web_scrapers/__init__.py
+++ python/web_scrapers/__init__.py
@@ -1,4 +1,5 @@
 from .hotels import HotelScraper
+from .restaurants import RestaurantScraper
 from .things_to_do import ThingsToDoScraper
 
-__all__ = ["HotelScraper", "ThingsToDoScraper"]
+__all__ = ["HotelScraper", "ThingsToDoScraper", "RestaurantScraper"]
