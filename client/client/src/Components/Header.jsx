1642b58e2f1d97e9c58f184e8a1fa9373c5595be
Emmanuel Chukwuemetam Michael <immanuel@meta.com>
2025-07-11 16:55:24 -0700

    deleted unnecessary comments

--- client/client/src/Components/Header.jsx
+++ client/client/src/Components/Header.jsx
@@ -1,9 +1,5 @@
-<<<<<<< Updated upstream
-import { Link } from 'react-router-dom';
-=======
 import { useNavigate } from 'react-router-dom';
 import Profile from './Profile.jsx';
->>>>>>> Stashed changes
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
