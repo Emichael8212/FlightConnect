7b7fd2727cc10afeb057a5c830681d68f5a1a626
Emmanuel Chukwuemetam Michael <immanuel@meta.com>
2025-07-21 18:13:21 -0700

    implemented a DRY fallback logic

--- server/utils/fallback.js
+++ server/utils/fallback.js
@@ -1,36 +1,41 @@
 import { PrismaClient } from '@prisma/client';
-
 const prisma = new PrismaClient();
 
+const modelLookup = {
+    'hotel': prisma.hotel,
+    'restaurant': prisma.restaurant,
+    'thingsToDo': prisma.thingsToDo,
+};
+
+function byRatingandReviewCount(a, b) {
+    const ratingA = a.rating ?? 0;
+    const ratingB = b.rating ?? 0;
+    if (ratingB !== ratingA) {
+    return ratingB - ratingA;
+    }
+    const reviewsA = a.reviewCount ?? 0;
+    const reviewsB = b.reviewCount ?? 0;
+    return reviewsB - reviewsA;
+}
 // fallback function
 // if the user's query is not in the database, return a list of items from the same city as the user's query
 export async function getFallbackItems(city, excludeCategory, neededCount) {
     const categoriesToFetch = ['hotel', 'restaurant', 'thingsToDo'].filter(category => category !== excludeCategory);
     // fetch items from each category
-    const results = await Promise.all(categoriesToFetch.map(async category => {
         // if the category is hotel, fetch hotels from the same city, as well as restaurants and things to do from the same city
-        if (category === 'hotel') {
-            return prisma.hotel.findMany({ where: { city: { equals: city, mode: 'insensitive' } }, take: 50 });
-        } else if (category === 'restaurant') {
-            return prisma.restaurant.findMany({ where: { city: { equals: city, mode: 'insensitive' } }, take: 50 });
-        } else if (category === 'thingsToDo') {
-            return prisma.thingsToDo.findMany({ where: { city: { equals: city, mode: 'insensitive' } }, take: 50 });
-        }
-        return [];
-    }));
-
+    const results = await Promise.all(
+        categoriesToFetch.map(category =>
+            modelLookup[category].findMany({
+                where: { city: {equals: city, mode: 'insensitive' } },
+                take: 50,
+            })
+        )
+    );
     // Flatten the array of arrays
     const allItems = results.flat();
 
     // sort the fallback items by rating first, then by review count
-    allItems.sort((a, b) => {
-        // then sort by rating
-        if ((b.rating || 0) !== (a.rating || 0)) {
-            return (b.rating || 0) - (a.rating || 0);
-        }
-        // in case of a tie, sort by review count
-        return (b.reviewCount || 0) - (a.reviewCount || 0);
-    });
+    allItems.sort(byRatingandReviewCount)
 
     // return the first neededCount items
     return allItems.slice(0, neededCount).map(item => ({
