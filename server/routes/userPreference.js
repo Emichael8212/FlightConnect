f743fb0ab8aafa732f5d073bd79d783b3917f217
Emmanuel Chukwuemetam Michael <immanuel@meta.com>
2025-07-24 02:12:56 -0700

    modified the user preference.js

--- server/routes/userPreference.js
+++ server/routes/userPreference.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import { authenticateToken } from '../routes/authMiddleware.js';
 import { PrismaClient } from '@prisma/client';
+import {
+  DEFAULT_MAIN_WEIGHTS,
+  DEFAULT_HOTEL_WEIGHTS,
+  DEFAULT_RESTAURANT_WEIGHTS,
+  DEFAULT_THINGS_TO_DO_WEIGHTS,
+} from '../../shared/WeightsConstants.mjs';
 
 const prisma = new PrismaClient();
 const router = express.Router();
@@ -22,7 +28,28 @@ router.get('/', authenticateToken, async (req, res) => {
 
 // post a new preference for a new user on registration
 router.post('/', authenticateToken, async (req, res) => {
-  const { budgetTier, cuisine, activityCategory, defaultCity } = req.body;
+  const {
+    budgetTier,
+    cuisine,
+    activityCategory,
+    defaultCity,
+
+    cityWeight,
+    budgetWeight,
+    cuisineWeight,
+    activityWeight,
+
+    hotelPriceWeight,
+    hotelRatingWeight,
+    hotelReviewWeight,
+
+    restaurantRatingWeight,
+    restaurantReviewWeight,
+
+    thingsToDoRatingWeight,
+    thingsToDoReviewWeight,
+   } = req.body;
+
   try {
     // if there's no perefence for the user, create a new one
     const userPref = await prisma.userPreference.upsert({
@@ -33,6 +60,19 @@ router.post('/', authenticateToken, async (req, res) => {
         cuisine,
         activityCategory,
         defaultCity,
+
+        // set default weights if not provided
+        cityWeight: cityWeight || DEFAULT_MAIN_WEIGHTS.cityWeight,
+        budgetWeight: budgetWeight || DEFAULT_MAIN_WEIGHTS.budgetWeight,
+        cuisineWeight: cuisineWeight || DEFAULT_MAIN_WEIGHTS.cuisineWeight,
+        activityWeight: activityWeight || DEFAULT_MAIN_WEIGHTS.activityWeight,
+        hotelPriceWeight: hotelPriceWeight || DEFAULT_HOTEL_WEIGHTS.priceWeight,
+        hotelRatingWeight: hotelRatingWeight || DEFAULT_HOTEL_WEIGHTS.ratingWeight,
+        hotelReviewWeight: hotelReviewWeight || DEFAULT_HOTEL_WEIGHTS.reviewWeight,
+        restaurantRatingWeight: restaurantRatingWeight || DEFAULT_RESTAURANT_WEIGHTS.ratingWeight,
+        restaurantReviewWeight: restaurantReviewWeight || DEFAULT_RESTAURANT_WEIGHTS.reviewWeight,
+        thingsToDoRatingWeight: thingsToDoRatingWeight || DEFAULT_THINGS_TO_DO_WEIGHTS.ratingWeight,
+        thingsToDoReviewWeight: thingsToDoReviewWeight || DEFAULT_THINGS_TO_DO_WEIGHTS.reviewWeight,
       },
       // if there's a preference for the user, I'll update it
       update: {
@@ -40,6 +80,19 @@ router.post('/', authenticateToken, async (req, res) => {
         cuisine,
         activityCategory,
         defaultCity,
+
+        // update weights if provided
+        cityWeight: cityWeight || undefined,
+        budgetWeight: budgetWeight || undefined,
+        cuisineWeight: cuisineWeight || undefined,
+        activityWeight: activityWeight || undefined,
+        hotelPriceWeight: hotelPriceWeight || undefined,
+        hotelRatingWeight: hotelRatingWeight || undefined,
+        hotelReviewWeight: hotelReviewWeight || undefined,
+        restaurantRatingWeight: restaurantRatingWeight || undefined,
+        restaurantReviewWeight: restaurantReviewWeight || undefined,
+        thingsToDoRatingWeight: thingsToDoRatingWeight || undefined,
+        thingsToDoReviewWeight: thingsToDoReviewWeight || undefined,
       },
     });
     // return the updated preference
